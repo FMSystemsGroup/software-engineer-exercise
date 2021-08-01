be9de51cf0e2e55f5499cbc138fa69915eaa2625
Willen Silva <willen.silva@dudesolutions.com>
2021-08-01 18:12:19 -0400

    Updating seed data.

--- FMSystems.WeatherForecast.Infrastructure/Db/SeedData/StaticCities.cs
+++ FMSystems.WeatherForecast.Infrastructure/Db/SeedData/StaticCities.cs
@@ -10,13 +10,13 @@ namespace FMSystems.WeatherForecast.Infrastructure.Db.SeedData
 {
     public static class StaticCities
     {
-        public static readonly City PhoenixAzUs = new City() { Id = 1, Name = "Phoenix", Country = "US", State = "AZ", Latitude = 33.448376, Longitude = -112.074036 };
+        public static readonly City PhoenixAzUs = new City() { Id = 1, Name = "Phoenix", Country = "United States", State = "AZ", Latitude = 33.448376, Longitude = -112.074036 };
 
-        public static readonly City RaleighNcUs = new City() { Id = 2, Name = "Raleigh", Country = "US", State = "NC", Latitude = 35.787743, Longitude = -78.644257 };
+        public static readonly City RaleighNcUs = new City() { Id = 2, Name = "Raleigh", Country = "United States", State = "NC", Latitude = 35.787743, Longitude = -78.644257 };
 
-        public static readonly City StJohnNbCa = new City() { Id = 3, Name = "Saint John", Country = "CA", State = "NB", Latitude = 45.273918, Longitude = -66.067657 };
+        public static readonly City StJohnNbCa = new City() { Id = 3, Name = "Saint John", Country = "Canada", State = "NB", Latitude = 45.273918, Longitude = -66.067657 };
 
-        public static readonly City SanDiegoCaUs = new City() { Id = 4, Name = "San Diego", Country = "US", State = "CA", Latitude = 32.715736, Longitude = -117.161087 };
+        public static readonly City SanDiegoCaUs = new City() { Id = 4, Name = "San Diego", Country = "United States", State = "CA", Latitude = 32.715736, Longitude = -117.161087 };
 
         public static IEnumerable<City> GetAll() =>
             new[]
