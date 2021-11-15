0fffa40954bca4a77dc5e6fd11c26997a5c64643
T <thomasdivinesmith@gmail.com>
2021-11-15 11:28:26 -0500

    API Tests added.

---
+++ FMS.API.Tests/CityServiceTests.cs
@@ -0,0 +1,40 @@
+using System.Linq;
+using FMS.API.Minimal.Repositories;
+using NUnit.Framework;
+
+namespace FMS.API.Tests;
+
+public class CityServiceTests
+{
+    [TestCase("Raleigh, NC", true)]
+    [TestCase("typo", false)]
+    [TestCase("", false)]
+    public void IsValidCity_GetCity_ReturnsTrue(string name, bool expected)
+    {
+        // arrange
+        CityRepository repo = new();
+        var list = repo.GetAll().ToList();
+
+        // act
+        bool result = list.Contains(name); 
+
+        // assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestCase(4, true)]
+    [TestCase(5, false)]
+    [TestCase(0, false)]
+    public void IsValidCount_GetCity_ReturnsTrue(int count, bool expected)
+    {
+        // arrange
+        CityRepository repo = new();
+        var list = repo.GetAll().ToList();
+
+        // act
+        bool result = list.Count() == count;
+
+        // assert
+        Assert.AreEqual(expected, result);
+    }
+}
