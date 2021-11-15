5c1d33b9f9dc2a192a3fdf0773fb8d8a8ebb404b
T <thomasdivinesmith@gmail.com>
2021-11-15 11:30:43 -0500

    Shared Class Library added.

---
+++ FMS.Shared/Models/WeatherForecast.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace FMS.Shared;
+
+public class WeatherForecast
+{
+    public WeatherForecast(DateTime? date)
+    {
+        Date = date;
+    }
+
+    public WeatherForecast(DateTime? date, string? description, string? temperature, string? uvIndex)
+    {
+        Date = date;
+        Description = description;
+        Temperature = temperature;
+        UVIndex = uvIndex;
+    }
+
+    public DateTime? Date { get; private set; }
+    public string? Description { get; private set; }
+    public string? Temperature { get; private set; }
+    public string? UVIndex { get; private set; }
+    public HttpStatusCode StatusCode { get; set; }
+}
